4e3c426c9ea4b1c48b7d895ac46882a71d827db3
STELIX <ssspinni@gmail.com>
2014-12-07 10:51:38 +0100

    Settings - Button settings: CheckBoxes to Switches
    Change-Id: Id72eec3ee2d5f9b90725c7787d21a34f8f58de57
    Signed-off-by: STELIX <ssspinni@gmail.com>
    Signed-off-by: iceandfire <arhamjamal@gmail.com>
    
    Conflicts:
    	res/xml/button_settings.xml
    	src/com/android/settings/ButtonSettings.java

--- src/com/android/settings/ButtonSettings.java
+++ src/com/android/settings/ButtonSettings.java
@@ -24,12 +24,12 @@ import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
 import android.os.RemoteException;
 import android.os.Handler;
-import android.preference.CheckBoxPreference;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceCategory;
 import android.preference.PreferenceManager;
 import android.preference.PreferenceScreen;
+import android.preference.SwitchPreference;
 import android.provider.Settings;
 
 import android.util.Log;
@@ -91,10 +91,10 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
     private ListPreference mHomeDoubleTapAction;
     private ListPreference mMenuPressAction;
     private ListPreference mMenuLongPressAction;
-	private CheckBoxPreference mDisableNavigationKeys;
-    private CheckBoxPreference mPowerEndCall;
-    private CheckBoxPreference mHomeAnswerCall;
-	private CheckBoxPreference mVolumeKeyWakeControl;
+	private SwitchPreference mDisableNavigationKeys;
+    private SwitchPreference mPowerEndCall;
+    private SwitchPreference mHomeAnswerCall;
+    private SwitchPreference mVolumeKeyWakeControl;
 
     private PreferenceCategory mNavigationPreferencesCat;
 
@@ -127,15 +127,15 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
                 (PreferenceCategory) prefScreen.findPreference(CATEGORY_MENU);
 
 		// Power button ends calls.
-        mPowerEndCall = (CheckBoxPreference) findPreference(KEY_POWER_END_CALL);
+        mPowerEndCall = (SwitchPreference) findPreference(KEY_POWER_END_CALL);
 
         // Home button answers calls.
-        mHomeAnswerCall = (CheckBoxPreference) findPreference(KEY_HOME_ANSWER_CALL);
+        mHomeAnswerCall = (SwitchPreference) findPreference(KEY_HOME_ANSWER_CALL);
 
         mHandler = new Handler();
 
 		// Force Navigation bar related options
-        mDisableNavigationKeys = (CheckBoxPreference) findPreference(DISABLE_NAV_KEYS);
+        mDisableNavigationKeys = (SwitchPreference) findPreference(DISABLE_NAV_KEYS);
 
         // Only visible on devices that does not have a navigation bar already,
         // and don't even try unless the existing keys can be disabled
@@ -223,7 +223,7 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
 		if (Utils.hasVolumeRocker(getActivity())) {
             int wakeControlAction = Settings.System.getInt(resolver,
                     Settings.System.VOLUME_WAKE_SCREEN, 0);
-            mVolumeKeyWakeControl = initCheckBox(KEY_VOLUME_WAKE_DEVICE, (wakeControlAction == 1));
+            mVolumeKeyWakeControl = initSwitch(KEY_VOLUME_WAKE_DEVICE, (wakeControlAction == 1));
         } else {
             prefScreen.removePreference(volumeCategory);
         }
@@ -254,17 +254,17 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
         }
     }
 
-	private CheckBoxPreference initCheckBox(String key, boolean checked) {
-        CheckBoxPreference checkBoxPreference = (CheckBoxPreference) getPreferenceManager()
+	private SwitchPreference initSwitch(String key, boolean checked) {
+        SwitchPreference switchPreference = (SwitchPreference) getPreferenceManager()
                 .findPreference(key);
-        if (checkBoxPreference != null) {
-            checkBoxPreference.setChecked(checked);
-            checkBoxPreference.setOnPreferenceChangeListener(this);
+        if (switchPreference != null) {
+            switchPreference.setChecked(checked);
+            switchPreference.setOnPreferenceChangeListener(this);
         }
-        return checkBoxPreference;
+        return switchPreference;
     }
 
-    private void handleCheckBoxChange(CheckBoxPreference pref, Object newValue, String setting) {
+    private void handleSwitchChange(SwitchPreference pref, Object newValue, String setting) {
         Boolean value = (Boolean) newValue;
         int intValue = (value) ? 1 : 0;
         Settings.System.putInt(getContentResolver(), setting, intValue);
@@ -304,7 +304,7 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
                     Settings.System.KEY_MENU_LONG_PRESS_ACTION);
             return true;
 		} else if (preference == mVolumeKeyWakeControl) {
-            handleCheckBoxChange(mVolumeKeyWakeControl, newValue,
+            handleSwitchChange(mVolumeKeyWakeControl, newValue,
                     Settings.System.VOLUME_WAKE_SCREEN);
             return true;
         }

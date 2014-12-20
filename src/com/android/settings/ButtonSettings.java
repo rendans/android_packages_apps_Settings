1789308214b54db5f0b23e44987ef001395c3929
Michael Bestas <mikeioannina@gmail.com>
2014-12-21 00:12:40 +0200

    Forward port 'Swap volume buttons' (3/3)
    Change-Id: I44a2b37b95848fff24c53c4c3561a3c000c487a1
    Signed-off-by: iceandfire <arhamjamal@gmail.com>
    
    Conflicts:
    	res/values/cm_strings.xml
    	res/xml/button_settings.xml
    	src/com/android/settings/ButtonSettings.java

--- src/com/android/settings/ButtonSettings.java
+++ src/com/android/settings/ButtonSettings.java
@@ -58,6 +58,7 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
 	private static final String DISABLE_NAV_KEYS = "disable_nav_keys";
 	private static final String KEY_POWER_END_CALL = "power_end_call";
     private static final String KEY_HOME_ANSWER_CALL = "home_answer_call";
+    private static final String KEY_SWAP_VOLUME_BUTTONS = "swap_volume_buttons";
 
     private static final String CATEGORY_POWER = "power_key";
     private static final String CATEGORY_HOME = "home_key";
@@ -100,6 +101,7 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
 	private ListPreference mAppSwitchPressAction;
     private ListPreference mAppSwitchLongPressAction;
 	private SwitchPreference mDisableNavigationKeys;
+    private SwitchPreference mSwapVolumeButtons;
     private SwitchPreference mPowerEndCall;
     private SwitchPreference mHomeAnswerCall;
 
@@ -275,6 +277,12 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
             if (!showVolumeWake) {
                 volumeCategory.removePreference(findPreference(Settings.System.VOLUME_WAKE_SCREEN));
             }
+
+            int swapVolumeKeys = Settings.System.getInt(getContentResolver(),
+                    Settings.System.SWAP_VOLUME_KEYS_ON_ROTATION, 0);
+            mSwapVolumeButtons = (SwitchPreference)
+                    prefScreen.findPreference(KEY_SWAP_VOLUME_BUTTONS);
+            mSwapVolumeButtons.setChecked(swapVolumeKeys > 0);
         } else {
             prefScreen.removePreference(volumeCategory);
         }
@@ -440,7 +448,12 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
 
     @Override
     public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
-        if (preference == mDisableNavigationKeys) {
+        if (preference == mSwapVolumeButtons) {
+            int value = mSwapVolumeButtons.isChecked()
+                    ? (Utils.isTablet(getActivity()) ? 2 : 1) : 0;
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.SWAP_VOLUME_KEYS_ON_ROTATION, value);
+        } else if (preference == mDisableNavigationKeys) {
             mDisableNavigationKeys.setEnabled(false);
             writeDisableNavkeysOption(getActivity(), mDisableNavigationKeys.isChecked());
             updateDisableNavkeysOption();

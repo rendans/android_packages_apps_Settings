1aac0d3099fcd1d544160eced44a8e2d71099bc3
Altaf-Mahdi <altaf.mahdi@gmail.com>
2015-01-22 13:26:47 +0000

    QS: add newly added tiles
    * Battery saver
    * Brightness
    * Expanded desktop
    * Screen off
    
    * also add notifications tile
    
    Change-Id: I64956333bd22d26db6ce72a6149b496d4542bfec
    Signed-off-by: iceandfire <arhamjamal@gmail.com>
    
    Conflicts:
    	res/values/custom_strings.xml
    	src/com/android/settings/SettingsActivity.java

--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -71,6 +71,7 @@ import com.android.settings.accessibility.AccessibilitySettings;
 import com.android.settings.accessibility.CaptionPropertiesFragment;
 import com.android.settings.accounts.AccountSettings;
 import com.android.settings.accounts.AccountSyncSettings;
+import com.android.settings.applications.ExpandedDesktopPreferenceFragment;
 import com.android.settings.applications.InstalledAppDetails;
 import com.android.settings.applications.ManageApplications;
 import com.android.settings.applications.ProcessStatsUi;
@@ -310,7 +311,8 @@ public class SettingsActivity extends Activity
             ApnSettings.class.getName(),
             BlacklistSettings.class.getName(),
             ProfilesSettings.class.getName(),
-            com.android.settings.cyanogenmod.PrivacySettings.class.getName()
+            com.android.settings.cyanogenmod.PrivacySettings.class.getName(),
+            ExpandedDesktopPreferenceFragment.class.getName()
     };
 
 

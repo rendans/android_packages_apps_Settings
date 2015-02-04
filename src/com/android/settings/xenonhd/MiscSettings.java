56889fd8d82ffbbf2b1710af497671d3ad746514
iceandfire <arhamjamal@gmail.com>
2015-02-04 21:18:47 +0530

    Move density settings from display settings to misc settings
    - Also remove the expanded settings option from display settings
    
    Change-Id: Id5ffbbdfcc9d00c2430758cad9a7607f60c5fe22
    Signed-off-by: iceandfire <arhamjamal@gmail.com>

--- src/com/android/settings/xenonhd/MiscSettings.java
+++ src/com/android/settings/xenonhd/MiscSettings.java
@@ -15,7 +15,16 @@
 */
 package com.android.settings.xenonhd;
 
+import android.app.Activity;
+import android.app.ActivityManagerNative;
+import android.app.Dialog;
+import android.app.IActivityManager;
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.RemoteException;
+import android.os.ServiceManager;
+import android.os.SystemProperties;
 import android.os.UserHandle;
 import android.content.ContentResolver;
 import android.content.Context;
@@ -29,6 +38,7 @@ import android.preference.SwitchPreference;
 import android.provider.Settings;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
@@ -37,11 +47,13 @@ import com.android.settings.R;
 
 public class MiscSettings extends SettingsPreferenceFragment implements
 Preference.OnPreferenceChangeListener {
+    private static final String TAG = "MiscSettings";
 
     private static final String SHOW_CLEAR_ALL_RECENTS = "show_clear_all_recents";
     private static final String RECENTS_CLEAR_ALL_LOCATION = "recents_clear_all_location";
     private static final String KEY_TOAST_ANIMATION = "toast_animation";
     private static final String FORCE_EXPANDED_NOTIFICATIONS = "force_expanded_notifications";
+    private static final String KEY_LCD_DENSITY = "lcd_density";
 
     private Context mContext;
 
@@ -49,6 +61,7 @@ Preference.OnPreferenceChangeListener {
     private ListPreference mRecentsClearAllLocation;
     private ListPreference mToastAnimation;
     private SwitchPreference mForceExpanded;
+    private ListPreference mLcdDensityPreference;
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -85,6 +98,30 @@ Preference.OnPreferenceChangeListener {
                 Settings.System.FORCE_EXPANDED_NOTIFICATIONS, 0, UserHandle.USER_CURRENT) == 1);
         mForceExpanded.setOnPreferenceChangeListener(this);
 
+        mLcdDensityPreference = (ListPreference) findPreference(KEY_LCD_DENSITY);
+        int defaultDensity = DisplayMetrics.DENSITY_DEVICE;
+        int currentDensity = DisplayMetrics.DENSITY_CURRENT;
+        int currentIndex = -1;
+        String[] densityEntries = new String[8];
+        for (int idx = 0; idx < 8; ++idx) {
+            int pct = (75 + idx*5);
+            int val = defaultDensity * pct / 100;
+            densityEntries[idx] = Integer.toString(val);
+            if (pct == 100) {
+                densityEntries[idx] += " (" + getResources().getString(R.string.lcd_density_default) + ")";
+            }
+            if (currentDensity == val) {
+                currentIndex = idx;
+            }
+        }
+        mLcdDensityPreference.setEntries(densityEntries);
+        mLcdDensityPreference.setEntryValues(densityEntries);
+        if (currentIndex != -1) {
+            mLcdDensityPreference.setValueIndex(currentIndex);
+        }
+        mLcdDensityPreference.setOnPreferenceChangeListener(this);
+        updateLcdDensityPreferenceDescription(currentDensity);
+
     }
 
     public boolean onPreferenceChange(Preference preference, Object objValue) {
@@ -110,6 +147,22 @@ Preference.OnPreferenceChangeListener {
             Settings.System.putIntForUser(getActivity().getContentResolver(),
             Settings.System.FORCE_EXPANDED_NOTIFICATIONS, show ? 1 : 0, UserHandle.USER_CURRENT);
             return true;
+        } else if (preference == mLcdDensityPreference) {
+            try {
+                // The value must begin with a decimal number.  It may
+                // optionally be follewed by a space and arbitrary text.
+                String strValue = (String) objValue;
+                int idx = strValue.indexOf(' ');
+                if (idx > 0) {
+                    strValue = strValue.substring(0, idx);
+                }
+                int value = Integer.parseInt(strValue);
+                writeLcdDensityPreference(preference.getContext(), value);
+                updateLcdDensityPreferenceDescription(value);
+            }
+            catch (NumberFormatException e) {
+                Log.e(TAG, "could not persist display density setting", e);
+            }
         }
         return false;
     }
@@ -142,6 +195,66 @@ Preference.OnPreferenceChangeListener {
         }
         if (mRecentsClearAllLocation != null && summary != -1) {
             mRecentsClearAllLocation.setSummary(res.getString(summary));
+        }
+    }
+
+    private void updateLcdDensityPreferenceDescription(int currentDensity) {
+        int defaultDensity = DisplayMetrics.DENSITY_DEVICE;
+        ListPreference preference = mLcdDensityPreference;
+        String summary;
+        if (currentDensity < 10 || currentDensity >= 1000) {
+            // Unsupported value
+            summary = getResources().getString(R.string.lcd_density_unsupported);
+        }
+        else {
+            summary = String.format(getResources().getString(R.string.lcd_density_summary),
+                    currentDensity);
+            if (currentDensity == defaultDensity) {
+                summary += " (" + getResources().getString(R.string.lcd_density_default) + ")";
+            }
+        }
+        preference.setSummary(summary);
+    }
+
+    public void writeLcdDensityPreference(final Context context, int value) {
+        try {
+            SystemProperties.set("persist.sys.lcd_density", Integer.toString(value));
+        }
+        catch (Exception e) {
+            Log.e(TAG, "Unable to save LCD density");
+            return;
+        }
+        final IActivityManager am = ActivityManagerNative.asInterface(ServiceManager.checkService("activity"));
+        if (am != null) {
+            AsyncTask<Void, Void, Void> task = new AsyncTask<Void, Void, Void>() {
+                @Override
+                protected void onPreExecute() {
+                    ProgressDialog dialog = new ProgressDialog(context);
+                    dialog.setMessage(getResources().getString(R.string.restarting_ui));
+                    dialog.setCancelable(false);
+                    dialog.setIndeterminate(true);
+                    dialog.show();
+                }
+                @Override
+                protected Void doInBackground(Void... arg0) {
+                    // Give the user a second to see the dialog
+                    try {
+                        Thread.sleep(1000);
+                    }
+                    catch (InterruptedException e) {
+                        // Ignore
+                    }
+                    // Restart the UI
+                    try {
+                        am.restart();
+                    }
+                    catch (RemoteException e) {
+                        Log.e(TAG, "Failed to restart");
+                    }
+                    return null;
+                }
+            };
+            task.execute((Void[])null);
         }
     }
 }

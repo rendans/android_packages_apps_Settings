9804c9fd06292bce3fed07517e744e13b2ac0bbf
Steve Kondik <steve@cyngn.com>
2015-02-18 03:44:31 -0800

    livedisplay: Add LiveDisplay settings
     * Add preference for LiveDisplay mode, and additional supporting
       options for auto mode.
     * CABC, SRE, CE are all under this umbrella now.
     * Manual RGB tweaking is now handled here and the requirement for
       CMHW has been removed as it will go thru RenderEngine.
     * Moved all related code into the livedisplay package.
    
    Change-Id: I0b00f2bfd8f8227d64ad271bbe0f004f0b4bcf9d
    Signed-off-by: iceandfire <arhamjamal@gmail.com>
    
    Conflicts:
    	res/values/cm_strings.xml
    	res/xml/display.xml
    	src/com/android/settings/DisplaySettings.java
    	src/com/android/settings/Settings.java
    	src/com/android/settings/SettingsActivity.java
    	src/com/android/settings/cyanogenmod/BootReceiver.java
    	src/com/android/settings/cyanogenmod/qs/QSTileHolder.java

--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -86,6 +86,7 @@ import com.android.settings.deviceinfo.Memory;
 import com.android.settings.deviceinfo.UsbSettings;
 import com.android.settings.fuelgauge.BatterySaverSettings;
 import com.android.settings.fuelgauge.PowerUsageSummary;
+import com.android.settings.livedisplay.LiveDisplay;
 import com.android.settings.notification.NotificationAppList;
 import com.android.settings.notification.OtherSoundSettings;
 import com.android.settings.profiles.NFCProfileTagCallback;
@@ -312,7 +313,8 @@ public class SettingsActivity extends Activity
             BlacklistSettings.class.getName(),
             ProfilesSettings.class.getName(),
             com.android.settings.cyanogenmod.PrivacySettings.class.getName(),
-            ExpandedDesktopPreferenceFragment.class.getName()
+            ExpandedDesktopPreferenceFragment.class.getName(),
+            LiveDisplay.class.getName()
     };
 
 

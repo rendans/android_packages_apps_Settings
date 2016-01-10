1462cfb933517183fa337bf7ddf9ef970b983b0f
rendan <rendans@gmail.com>
2016-01-10 18:46:46 +0700

    Add Layers Shortcut into Settings Dashboard

--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -1315,6 +1315,16 @@ public class SettingsActivity extends Activity
                     }
                     if (!supported) {
                         removeTile = true;
+                  } 
+                  
+                } else if (id == R.id.bitsyko_layers) {
+                    boolean supported = false;
+                    try {
+                        supported = (getPackageManager().getPackageInfo("com.lovejoy777.rroandlayersmanager", 0).versionCode > 0);
+                    } catch (PackageManager.NameNotFoundException e) {
+                    }
+                    if (!supported) {
+                        removeTile = true;
                     }
                 }
 
